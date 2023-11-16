e2337aff1594a7020a93f42fac89bfa165687a13
Simon Peier <simon.w.peier@gmail.com>
2023-11-16 15:42:50 +0100

    Implement basic layout

--- app/src/main/java/ch/simonpeier/weatherapp/MainActivity.kt
+++ app/src/main/java/ch/simonpeier/weatherapp/MainActivity.kt
@@ -3,13 +3,17 @@ package ch.simonpeier.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ch.simonpeier.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WeatherLayout("Android")
                 }
             }
         }
@@ -30,17 +34,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun WeatherLayout(name: String, modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(48.dp)
+    ) {
+        Text(text = "Location", fontSize = 48.sp)
+        Text(text = "Date", fontSize = 32.sp, modifier = Modifier.padding(bottom = 40.dp))
+        Text(text = "-°", fontSize = 48.sp, modifier = Modifier.padding(bottom = 40.dp))
+        WeatherInfoCard(title = "Air", firstContent = "Pressure", secondContent = "Humidity")
+        WeatherInfoCard(title = "Wind", firstContent = "Speed", secondContent = "Direction")
+    }
+}
+
+@Composable
+fun WeatherInfoCard(
+    title: String,
+    firstContent: String,
+    secondContent: String,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(bottom = 32.dp)) {
+        Text(text = title, fontSize = 32.sp)
+        Text(text = firstContent, fontSize = 24.sp)
+        Text(text = secondContent, fontSize = 24.sp)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WeatherLayoutPreview() {
     WeatherAppTheme {
-        Greeting("Android")
+        WeatherLayout("Android")
     }
 }
